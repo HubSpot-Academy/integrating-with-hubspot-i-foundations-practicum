c92f158161c13cde8f1e3488df36df9b6a9bc12e
Michelle Tabor <michelle.tabor@lynton.com>
2023-05-16 21:46:29 -0500

    added styling to root

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,11 +9,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.HAPIKEY;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    const plants = "https://api.hubspot.com/crm/v3/objects/2-14943023?limit=10&properties=acquired_date&properties=age&properties=scientific_name&properties=name&archived=false"
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json' 
+    }
+    try{
+        const resp = await axios.get(plants, { headers });
+        const data = resp.data.results;
+        res.render('plants', { title: 'Plants | HubSpot APIs', data })
+    } catch (error){
+        console.error(error);
+    }
+})
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -22,49 +37,48 @@ const PRIVATE_APP_ACCESS = '';
 
 // * Code for Route 3 goes here
 
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
+/** This is sample code to give you a reference for how you should structure your calls. */
 
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
+// App.get sample
+// app.get('/contacts', async (req, res) => {
+//     const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+//     const headers = {
+//         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+//         'Content-Type': 'application/json'
+//     }
+//     try {
+//         const resp = await axios.get(contacts, { headers });
+//         const data = resp.data.results;
+//         res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+//     } catch (error) {
+//         console.error(error);
+//     }
+// });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
+// app.post sample
+// app.post('/update', async (req, res) => {
+//     const update = {
+//         properties: {
+//             "favorite_book": req.body.newVal
+//         }
+//     }
 
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
+//     const email = req.query.email;
+//     const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+//     const headers = {
+//         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+//         'Content-Type': 'application/json'
+//     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
+//     try { 
+//         await axios.patch(updateContact, update, { headers } );
+//         res.redirect('back');
+//     } catch(err) {
+//         console.error(err);
+//     }
 
-});
-*/
+// });
+
 
 
 // * Localhost
