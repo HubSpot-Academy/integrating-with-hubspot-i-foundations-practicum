ac8c7de498339124398fc96574158347244e0ec5
Michelle Tabor <michelle.tabor@lynton.com>
2023-05-16 21:58:17 -0500

    added update route

--- index.js
+++ index.js
@@ -31,8 +31,9 @@ app.get('/', async (req, res) => {
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
-
+app.get('/update-cobj', (req, res) => {
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' })
+})
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
