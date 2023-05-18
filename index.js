a2655f1e969408792088da19da1a06b00e66d1c6
romerocam <romerocam@hotmail.com>
2023-05-18 10:25:19 -0300

    Private app access token

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require ('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-na1-684b78ea-7959-4374-babf-6da5328ca0d1';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
