0d95e7dae7a27f0c0c6930d97b9b7195bf6ddf22
coreyloftus <112141387+coreyloftus@users.noreply.github.com>
2023-05-25 12:39:34 -0400

    Index route created, cobj get and post routes stubbed

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
+const express = require("express")
+const axios = require("axios")
+const app = express()
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.set("view engine", "pug")
+app.use(express.static(__dirname + "/public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = ""
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get("/", async (req, res) => {
+    const message = { message: "Hello HubSpot Certification Giver being!" }
+    res.json(message)
+})
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
-
+app.get("/update-cobj", async (req, res) => {})
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
-
+app.post("/update-cobj", async (req, res) => {})
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
@@ -66,6 +70,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"))
