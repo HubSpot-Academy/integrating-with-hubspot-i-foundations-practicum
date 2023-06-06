086a46ee54b15aa5a53f257b35a6e97832836c0f
Peter Parker <peter@sprint-education.co.uk>
2023-06-06 14:48:43 +0100

    delete

--- index.js
+++ index.js
@@ -86,49 +86,17 @@ app.post("/update-cobj", async (req, res) => {
   }
 });
 
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
+/** using get to delete is far from ideal but saves me learning any more about pug */
+app.get("/delete-cobj", async (req, res) => {
+  const url = "https://api.hubspot.com/crm/v3/objects/games/" + req.query.id;
 
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    await axios.delete(url, { headers });
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
+  }
 });
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
-*/
 
 // * Localhost
 app.listen(3000, () => console.log("Listening on http://localhost:3000"));
