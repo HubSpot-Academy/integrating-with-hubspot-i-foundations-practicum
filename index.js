e8ab8201b29ca108593411d45110b5ccb54655dc
Peter Parker <peter@sprint-education.co.uk>
2023-06-06 12:57:37 +0100

    display a game

--- index.js
+++ index.js
@@ -34,7 +34,19 @@ app.get("/", async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
-app.get("/update-cobj", async (req, res) => {});
+app.get("/update-cobj", async (req, res) => {
+  const data = {
+    name: "test",
+    publisher: "pub",
+    rating: 1,
+  };
+  res.render("update", {
+    title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+    name: "test",
+    publisher: "pub",
+    rating: 1,
+  });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
