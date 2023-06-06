303243e14f366e79bd84a6fa0276dd59fd62c916
Peter Parker <peter@sprint-education.co.uk>
2023-06-06 11:21:37 +0100

    call clicks endpoint and view

--- index.js
+++ index.js
@@ -13,14 +13,30 @@ const PRIVATE_APP_ACCESS = "pat-na1-bf8e5671-ddc8-4631-a9ab-bbb199798e62";
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get("/", async (req, res) => {
+  const contacts = "https://api.hubspot.com/crm/v3/objects/clicks";
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const resp = await axios.get(contacts, { headers });
+    const data = resp.data.results;
+    res.render("clicks", { title: "Clicks | HubSpot APIs", data });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get("/update-cobj", async (req, res) => {});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post("/update-cobj", async (req, res) => {});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
