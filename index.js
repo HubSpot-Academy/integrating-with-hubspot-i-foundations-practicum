a4785bc5cd8e85f1aec6596bb13b88b4c3a1eebb
achodeveloper <acho.developer@gmail.com>
2023-08-10 09:56:29 -0700

    add routes and private token
    add private token and three routes as described in the instructions:
    
    (1) an app.get for the homepage ("/")
    
    (2) an app.get for rendering the form in a pug template (“/update-cobj”)
    
    (3) and an app.post for the route ("/update-cobj") that sends along the data captured by the form

--- index.js
+++ index.js
@@ -3,25 +3,37 @@ const axios = require('axios');
 const app = express();
 
 app.set('view engine', 'pug');
+
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-na1-3e5e4fde-a7c7-4855-944c-5c271f730920';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', (req, res) => {
 
+
+
+})
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
 
+
+})
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post('/update-cobj', (req, res) => {
 
+
+})
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
