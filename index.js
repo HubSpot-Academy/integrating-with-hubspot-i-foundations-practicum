6baa4e2a4752593cc3a310e1e873fa14a9f1811d
achodeveloper <acho.developer@gmail.com>
2023-08-10 10:27:09 -0700

    cleaned up updates pug and more
    and reverse get route functions, they got transposed. f1(a) f2(b) should be f1(b) f2(a)

--- index.js
+++ index.js
@@ -9,13 +9,25 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = 'pat-na1-3e5e4fde-a7c7-4855-944c-5c271f730920';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
 app.get('/', (req, res) => {
+
+    const partners = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(partners, { headers });
+        const data = resp.data.results;
+        res.render('updates', { pageTitle: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });
+    } catch (error) {
+        console.error(error);
+    }
 
 
 
@@ -25,18 +37,7 @@ app.get('/', (req, res) => {
 // * Code for Route 2 goes here
 app.get('/update-cobj', async (req, res) => {
 
-        const partners = 'https://api.hubspot.com/crm/v3/objects/contacts';
-        const headers = {
-            Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-            'Content-Type': 'application/json'
-        }
-        try {
-            const resp = await axios.get(partners, { headers });
-            const data = resp.data.results;
-            res.render('updates', { pageTitle: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });
-        } catch (error) {
-            console.error(error);
-        }
+    res.render('homepage', { customObjects: /* List of custom objects */ });
 
 
 })
