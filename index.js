0893a1ab5b33a196cf09c5199ee64c98ef8e85b0
achodeveloper <acho.developer@gmail.com>
2023-08-10 10:00:21 -0700

    defined updates.pug and rendered it with title
    Created a new pug template called updates in the views folder. Then, inside of this route, rendered the updates template and passed along a page title called Update Custom Object Form | Integrating With HubSpot I Practicum.

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.get('/', (req, res) => {
 // * Code for Route 2 goes here
 app.get('/update-cobj', (req, res) => {
 
+    res.render('updates', { pageTitle: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
 
 })
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
