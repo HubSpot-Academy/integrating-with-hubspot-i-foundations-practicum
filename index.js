6c2c51a7acb6708af7c02ebd2d9c0a1f2d665043
AdamSixandFlow.com <adam@sixandflow.com>
2023-09-08 17:58:21 +0100

    add updates pug and route 2

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get('/', async (req, res) => {
          const resp = await axios.get(successionCharacters, { headers });
          const data = resp.data.results;
          console.log(data)
-         res.render('successionCharacters', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data });      
+         res.render('successionCharacters', { title: 'Succession Characters | Integrating With HubSpot I Practicum', data });      
      } catch (error) {
          console.error(error);
      }
@@ -36,6 +36,13 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.get('/updates', async (req, res) => {
+    try {
+        res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
