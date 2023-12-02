c919448aa30cb6ddd1e20204ce030cd220fe0bd4
Martin Akra <martinaakra@gmail.com>
2023-12-02 14:18:39 +0100

    Added env variable and fetched it in the index.js file. dotenv dependency added to package.json

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
+// Grab API key from .env file in the root folder of this project
+require('dotenv').config()
+
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.API_KEY;
+console.log(PRIVATE_APP_ACCESS);
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. 
+// Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. 
+// Send this data along in the next route.
 
 // * Code for Route 2 goes here
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. 
+// Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
 
