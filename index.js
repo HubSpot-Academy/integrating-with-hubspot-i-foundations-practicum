8c4c6258af0a120c6886ecf2fdd21a607d04aad4
ghayoorhaider <ghayoor@learners.ai>
2023-12-04 08:28:40 -0500

    Added Api to get custom object records and added a templte to show these records in a table view. Also added some stylings

--- index.js
+++ index.js
@@ -8,11 +8,21 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'Bearer pat-eu1-a6f3ccc7-44ff-483f-80d9-9d94bb41a748';
+const API_URL = 'https://api.hubapi.com/crm/v3/objects'
+const HEADER = { 'Authorization': PRIVATE_APP_ACCESS }
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+
+app.get('/', async (req, res) => {
+    url = `${API_URL}/vehicles/?properties=name&properties=type&properties=registration_number&properties=brand`
+    const response = await axios.get(url, { headers: HEADER });
+    console.log('response', response.data.results[0].properties)
+    res.render('records', { title: 'Vehicles Data', vehicles: response.data.results });
+
+})
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
