7d1fe798960916255842fb3211bcb48c72925aae
Nagaraj0229 <npnagu50@gmail.com>
2023-12-18 15:08:54 +0530

    src: update overall tasks with this single commit

--- index.js
+++ index.js
@@ -1,71 +1,73 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
+require("dotenv").config();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-na1-f4afd4e6-49df-42ac-bcbd-0ff6bb8cff45'
+const HUBSPOT_BASE_URI ='https://api.hubspot.com/crm/v3';
+const PORT = 3000;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+const baseURI = `http://localhost:${PORT}`;
 
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
+app.get("/", async (_req, res) => {
+  const customObject =
+    `${HUBSPOT_BASE_URI}/objects/pets?properties=name,bio,pet_color,pet_country,pet_owner_address,pet_owner_name,pet_owner_phone_number`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const resp = await axios.get(customObject, { headers });
+    const data = resp.data.results;
+    res.render("homepage", { baseURI: baseURI + "/update-cobj", data });
+  } catch (e) {
+    console.error(e?.message);
+    res.render("error");
+  }
 });
 
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+app.get("/update-cobj", async (_req, res) => {
+  try {
+    res.render("update", { baseURI: baseURI + "/update-cobj" });
+  } catch (e) {
+    console.error(e.message);
+    res.render("error");
+  }
 });
-*/
 
+app.post("/update-cobj", async (req, res) => {
+  const updateProp = {
+    properties: {
+      name: req?.body?.name ?? '',
+      bio: req?.body?.bio ?? '',
+      pet_color: req?.body?.pet_color ?? '',
+      pet_country: req?.body?.pet_country ?? '',
+      pet_owner_address: req?.body?.pet_owner_address ?? '',
+      pet_owner_name: req?.body?.pet_owner_name ?? '',
+      pet_owner_phone_number: req?.body?.pet_owner_phone_number ?? '',
+    },
+  };
+  const updateCustomObject = `${HUBSPOT_BASE_URI}/objects/pets`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    await axios.post(updateCustomObject, updateProp, { headers });
+    res.redirect("/");
+  } catch (e) {
+    console.error(e.message);
+    res.redirect("/");
+  }
+});
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.use((_req, res, next) => {
+  res.render("error", { baseURI });
+});
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
