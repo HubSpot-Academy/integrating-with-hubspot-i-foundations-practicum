048c934b2405b071df5049d4e90e637329e14e17
Martin Akra <martinaakra@gmail.com>
2023-12-20 12:38:49 +0100

    Added new route for rendering HTML form in the index file, and added a new pug template for the form page for /update-cobj. Small styling changes applied to the .css for the form

--- index.js
+++ index.js
@@ -41,6 +41,11 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. 
 // Send this data along in the next route.
 
+app.get('/update-cobj', async (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum.' });
+    
+})
+
 // * Code for Route 2 goes here
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. 
