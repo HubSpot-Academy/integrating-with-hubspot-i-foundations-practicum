544f1dc6d95b95380b3c3cb9419ecea86f172446
Matt <matt@bridgerev.com>
2023-12-21 15:40:04 -0600

    Create get /update-cobj route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
+app.set('views', `${__dirname}/views`);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -16,7 +17,9 @@ const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });  
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
