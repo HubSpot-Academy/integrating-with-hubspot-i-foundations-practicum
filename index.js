9faae63665a3dcaa18e49be3e1b13fb4593f1fa5
Dennis Hickox <frozzel@me.com>
2024-01-03 12:43:26 -0500

    tested route and added properties of pet, modified pug view to display data from api request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,11 +9,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+
+// const PRIVATE_APP_ACCESS = '';// use process.env.PRIVATE_APP_ACCESS to access this variable create a .env file and add PRIVATE_APP_ACCESS=your_token_here to the file
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+
+app.get('/pets', async (req, res) => {
+
+    const pets = 'https://api.hubapi.com/crm/v3/objects/pets?properties=name&properties=age&properties=type';
+    const headers = {
+        Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const resp = await axios.get(pets, { headers });
+        const data = resp.data.results;
+        res.render('contacts', { title: 'Pets | HubSpot APIs', data });    
+        // res.json(data);  
+    } catch (error) {
+        console.error(error);
+    }
+
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -71,4 +92,9 @@ app.post('/update', async (req, res) => {
 
 PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log(`Listening on http://localhost:${PORT}`));
+app.listen(3000, () => {
+    console.log(`................................................`)
+    console.log(`🚀  Server running on http://localhost:${PORT}, 🚀`)
+    console.log(`...............................................`)
+}
+);
