85fefc226ecb347e5c17c4e7b2ea160cf9c71c00
Dennis Hickox <frozzel@me.com>
2024-01-03 13:28:46 -0500

    added nodemon and dotenv for testing and api key protection

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+require('dotenv').config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,7 +9,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+
+// const PRIVATE_APP_ACCESS = '';// use process.env.PRIVATE_APP_ACCESS to access this variable create a .env file and add PRIVATE_APP_ACCESS=your_token_here to the file
+
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -67,5 +70,14 @@ app.post('/update', async (req, res) => {
 */
 
 
+
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+
+PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => {
+    console.log(`................................................`)
+    console.log(`🚀  Server running on http://localhost:${PORT}, 🚀`)
+    console.log(`...............................................`)
+}
+);
