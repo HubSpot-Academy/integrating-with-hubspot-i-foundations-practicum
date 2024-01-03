34226f006725d8202cf3c4aa7c479169cf191a93
Christian Ross <rossc@globalia.ca>
2024-01-03 13:58:23 -0500

    [Added] private app token

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-na1-10ba3975-2c84-47c6-b0e8-fae16bf3efe8';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
