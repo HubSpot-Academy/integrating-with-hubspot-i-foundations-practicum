0ccef743576dabed7aa6a5d43b21cacc0bdb8598
Scott Marion <scott@threadgroup.com>
2024-01-20 11:12:58 -0500

    Homepage created, styles updated

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,11 +10,33 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/', async (req, res) => {
+    const obj_id = '2-22773143';
+    const obj_type = 'p44983171_lego_kits';
+    const legokits = 'https://api.hubspot.com/crm/v3/objects/' + obj_type;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    const params = {};
+    params.properties = "name,category,price,number_of_pieces,age_range";
+    const config = {};
+    config.headers = headers;
+    config.params = params;
 
-// * Code for Route 1 goes here
+    try {
+        const resp = await axios.get(legokits, config);
+        const data = resp.data.results;
+        //res.json(data);
+        res.render('homepage', { title: 'Home | HubSpot APIs', data });
+    } catch (error) {
+        console.error(error);
+    }
+
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
