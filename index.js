52e436383626f1377e645ee94e722510783f63d9
Tavis <117163005+tavisddavid@users.noreply.github.com>
2024-01-22 13:40:44 +0100

    Add create route for adding a new bike to the table

--- index.js
+++ index.js
@@ -1,83 +1,184 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = 'pat-eu1-0ee2eeb9-8e81-492a-8628-e7a7b49a9d19';
+const PRIVATE_APP_TOKEN = "pat-eu1-0ee2eeb9-8e81-492a-8628-e7a7b49a9d19";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-app.get('/homepage', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get("/", async (req, res) => {
+  const objectType = "2-114716868";
+  const properties = [
+    "bike_name",
+    "model",
+    "country_of_manufacture",
+    "name"
+  ];
+
+  const bikes = `https://api.hubspot.com/crm/v3/objects/${objectType}?properties=${properties}`;
+
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const response = await axios.get(bikes, { headers });
+    const data = response.data.results;
+
+    res.render("homepage", { title: "Homepage", data });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// TODO: ROUTE 2 - Create a new app.get route for the form to update new custom object data. Send this data along in the next route.
+
+app.get("/updates", async (req, res) => {
+  const objectType = "2-114716868";
+  const properties = [
+    "bike_name",
+    "model",
+    "country_of_manufacture",
+    "name"
+  ];
+
+  let objectId = req.query.id;
+
+  const getBike = `https://api.hubapi.com/crm/v3/objects/${objectType}/${objectId}?properties=${properties}`;
+
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    const response = await axios.get(getBike, { headers });
+    const data = response.data;
+
+    // res.json(data);
+    res.render("updates", {
+      title: "Update Custom Object Form | Integrating With HubSpot I Practicum",
+      bikeId: data.id,
+      bikeName: data.properties.bike_name,
+      bikeModel: data.properties.model,
+      countryOfManufacture: data.properties.country_of_manufacture,
+      name: data.properties.name,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to update your custom object data. Once executed, redirect the user to the homepage.
+
+app.post("/updates", async (req, res) => {
+  const objectType = "2-114716868";
+  const properties = [
+    "bike_name",
+    "model",
+    "country_of_manufacture",
+    "name"
+  ];
+
+  const update = {
+    properties: {
+      model: req.body.newModel,
+      country_of_manufacture: req.body.newCountry,
+      name: req.body.newName,
+    },
+  };
+
+  let objectId = req.query.id;
+
+  const updateBike = `https://api.hubapi.com/crm/v3/objects/${objectType}/${objectId}?properties=${properties}`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
+    "Content-Type": "application/json",
+  };
+  try {
+    await axios.patch(updateBike, update, { headers });
+
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+// TODO: ROUTE 4 - Create a new app.get route for the form to create a new custom object data. Send this data along in the next route.
+
+app.get("/create", async (req, res) => {
+    const objectType = "2-114716868";
+    const properties = [
+      "bike_name",
+      "model",
+      "country_of_manufacture",
+      "name"
+    ];
+  
+    const createBike = `https://api.hubapi.com/crm/v3/objects/${objectType}?properties=${properties}`;
+  
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
+      Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
+      "Content-Type": "application/json",
+    };
     try {
-        const response = await axios.get(contacts, { headers });
-        const data = response.data.results;
-        res.render('homepage', { title: 'Homepage', data});
+      const response = await axios.get(createBike, { headers });
+      const data = response.data;
+  
+      // res.json(data);
+      res.render("create", {
+        title: "Create Custom Object Form | Integrating With HubSpot I Practicum",
+        bike_name: req.body.bikeName,
+        model: req.body.bikeModel,
+        country_of_manufacture: req.body.bikeCountry,
+        name: req.body.bikeOwner,
+      });
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-});
+  });
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+// TODO: ROUTE 5 - Create a new app.post route for the custom objects form to create your custom object data. Once executed, redirect the user to the homepage.
 
-app.get('/updates', async (req, res) => {
-    const email = req.query.email;
-    const contactId = 101
-    
-    const url = `https://api.hubapi.com/crm/v3/objects/contacts/${contactId}?associations=bikes`;
-    
+app.post("/create", async (req, res) => {
+    const objectType = "2-114716868";
+    const properties = [
+      "bike_name",
+      "model",
+      "country_of_manufacture",
+      "name"
+    ];
+  
+    const create = {
+      properties: {
+        bike_name: req.body.bikeName,
+        model: req.body.bikeModel,
+        country_of_manufacture: req.body.bikeCountry,
+        name: req.body.bikeOwner,
+      },
+    };
+  
+    const createBike = `https://api.hubapi.com/crm/v3/objects/${objectType}?properties=${properties}`;
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const response = await axios.get(url, { headers });
-        const data = response.data;
-        
-        let contactBike = "";
-        
-        for (const [key, value] of Object.entries(data.associations)) {
-            console.log(data.associations[key])
-            contactBike = data.associations[key].results[0];
-          }
+      Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
+      "Content-Type": "application/json",
+    };
 
-        res.render('updates', {title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', userFirstname: data.properties.firstname, userLastname: data.properties.lastname, userEmail: data.properties.email, bikeName: contactBike});
+    try {
+      await axios.post(createBike, create, { headers });
+  
+      res.redirect("/");
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-});
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-app.post('/updates', async (req, res) => {
-    const update = {
-        associations: {
-            "bike_name": req.body.newVal  
-        }
-    }
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(error) {
-        console.error(error);
-    }
-});
-
-
+  });
+  
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
+
+
