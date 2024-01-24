b258a8235bb32e83e8f36858740efcec677ec20e
creativeskills <pramod@creativeskills.com>
2024-01-24 16:00:31 +0530

    List / Create Object with API integration completed

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-https://app.hubspot.com/contacts/44960682/objects/2-22899473/views/all/list
+https://app.hubspot.com/contacts/44960682/objects/2-22899981/views/all/list
--- index.js
+++ index.js
@@ -1,71 +1,56 @@
-const express = require('express');
-const axios = require('axios');
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+app.get("/", async (req, res) => {
+  try {
+    const apiURL = `https://api.hubapi.com/crm/v3/objects/tours?properties=tour_name,tour_description,tour_date,tour_cost,tour_contact_number`;
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      "Content-Type": "application/json",
     };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
+    const response = await axios.get(apiURL, { headers });
+    const tours = response.data.results;
+    res.render("home", { title: "Tour Plans", tours });
+  } catch (error) {
+    console.error(error);
+  }
 });
-*/
 
+app.get("/create", async (req, res) => {
+  res.render("create", { title: "Add Tour" });
+});
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.post("/create", async (req, res) => {
+  const data = {
+    properties: {
+      tour_name: req.body.tour_name,
+      tour_description: req.body.tour_description,
+      tour_date: req.body.tour_date,
+      tour_cost: req.body.tour_cost,
+      tour_contact_number: req.body.tour_contact_number,
+    },
+  };
+
+  const apiURL = `https://api.hubapi.com/crm/v3/objects/tours`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+
+  try {
+    await axios.post(apiURL, data, { headers });
+    res.redirect("/");
+  } catch (error) {
+    console.error(error);
+  }
+});
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
