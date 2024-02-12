cfd8fb0016defd3b4907d763cff6ced50b9e76ab
Daniel C <2736635+bitclaw@users.noreply.github.com>
2024-02-11 23:47:55 -0500

    daniel-chaves-iwh-i-practicum
    * 10. Next, let’s focus on the code you’ll write inside of the app.post route.
    
      - Make a POST request with the HTML form data to create a new CRM record.
      - After the CRM record is created, write a redirect back to the homepage.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
@@ -31,7 +32,28 @@ app.get('/update-cobj', async (req, res) => {
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 app.post('/update-cobj', async (req, res) => {
-    res.json({success: true})
+    const { name, short_address, street_number } = req.body;
+    const customObjectData = {
+        properties: {
+            name,
+            short_address,
+            street_number,
+        }
+    };
+
+    const createOrUpdateURL = 'https://api.hubspot.com/crm/v3/objects/2-23781024';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        await axios.post(createOrUpdateURL, customObjectData, { headers });
+        res.redirect('/'); // Redirect back to the homepage after successful update
+    } catch (error) {
+        console.error('Failed to create or update custom object:', error);
+        res.status(500).send('Error updating custom object');
+    }
 });
 
 /** 
