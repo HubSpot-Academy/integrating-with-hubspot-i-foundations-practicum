61989041d2054587b02b0f414482ebb7044964f1
Daniel C <2736635+bitclaw@users.noreply.github.com>
2024-02-12 00:18:53 -0500

    daniel-chaves-iwh-i-practicum
    * Pass in the correct url to the pug template
    * Remove oauth code since we are using access token authentication instead
    * Add URL to create / update the real estate listing

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 app.get('/', async (req, res) => {
-    res.render('home', {})
+    res.render('home', {
+        url: '/update-cobj',
+    })
 });
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
@@ -26,7 +28,7 @@ app.get('/update-cobj', async (req, res) => {
         url: '/',
         data
     }
-    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data })
+    res.render('updates', options)
 });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
