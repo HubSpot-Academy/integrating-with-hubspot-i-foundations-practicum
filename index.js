0f11d1be199dc7e85b4f64caf488e4fba3850a2f
Daniel C <2736635+bitclaw@users.noreply.github.com>
2024-02-11 23:10:41 -0500

    daniel-chaves-iwh-i-practicum
    * 8. In the index.js file, you’ll create three routes:
    
      - An app.get for the homepage ("/").
      - An app.get for rendering the HTML form in a pug template called updates. (“/update-cobj”)
      - The HTML form should have fields to create a new CRM record with the three custom properties you created.
      An app.post for the route ("/update-cobj") that sends along the data captured by the HTML form.

--- index.js
+++ index.js
@@ -12,15 +12,21 @@ const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    res.render('home', {})
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get('/update-cobj', async (req, res) => {
+    res.render('updates', {})
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post('/update-cobj', async (req, res) => {
+    res.json({success: true})
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
