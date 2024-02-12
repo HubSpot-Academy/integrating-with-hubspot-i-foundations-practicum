40acd50aa77e25aa42cd8c644029bc30aadf137f
Daniel C <2736635+bitclaw@users.noreply.github.com>
2024-02-11 23:19:00 -0500

    daniel-chaves-iwh-i-practicum
    * 9. First, create an app.get route for “/update-cobj”.
    
      - Create a new pug template called updates in the views folder.
      - Render the updates template and pass along a page title called Update Custom Object Form | Integrating With HubSpot I Practicum.
      - In the updates pug template, add a link “Return to the homepage” that links to the root route.

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ app.get('/', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 app.get('/update-cobj', async (req, res) => {
-    res.render('updates', {})
+    const data = {}
+    const options = {
+        title: 'Update Custom Object Form | Integrating With HubSpot I Practicum',
+        url: '/',
+        data
+    }
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum', data })
 });
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
