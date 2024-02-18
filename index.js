58507828b2cf6683487deca2fe5463de1c7960af
Jan Prudil <jan.prudil@gmail.com>
2024-02-18 22:30:38 +0100

    Index js - routes created

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,19 +9,31 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get( '/' , async (req, res) => {
+
+    res.render('dogs', {title: 'Dog directory'});
+
+});
+
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
-// * Code for Route 2 goes here
+app.get( '/update-cobj' , async (req, res) => {
+
+    
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post( '/update-cobj' , async (req, res) => {
+
+    
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
