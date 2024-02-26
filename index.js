dec2dec8a6b0dfc9e25d5faf0b885a7c94519bf4
Suraj Darekar <140320836+Suraj-Darekar@users.noreply.github.com>
2024-02-27 01:08:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,70 +2,57 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug'); // Set the view engine to Pug
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-na1-801b84ff-16be-499d-ba99-069267968fef'; // Your private app access token goes here
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
-
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+// Route 1 - Fetch data of VideoGame custom object and render it on the homepage
+app.get('/', async (req, res) => {
+    const videoGamesEndpoint = 'https://api.hubapi.com/crm/v3/objects/VideoGame?properties=name,bio,game';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
+      Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    try {
+      const resp = await axios.get(videoGamesEndpoint, { headers });
+      const videoGamesData = resp.data.results;
+      res.render('homepage', { title: 'Custom Object Table', videoGamesData });
+      //res.json(videoGamesData);
+    } catch (error) {
+      console.error(error);
     }
+  });
 
+// Route 2 - Render the form for creating or updating VideoGame custom object
+app.get('/update-cobj', (req, res) => {
+  res.render('updates.pug', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
 });
-*/
 
+// Route 3 - Handle form submission to create or update VideoGame custom object
+app.post('/update-cobj', async (req, res) => {
+  const { name, bio, game } = req.body;
+  const newVideoGame = {
+    properties: { name, bio, game }
+  };
 
-// * Localhost
+  const createVideoGameEndpoint = 'https://api.hubspot.com/crm/v3/objects/VideoGame';
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json'
+  };
+
+  try {
+    await axios.post(createVideoGameEndpoint, newVideoGame, { headers });
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error creating or updating Video Game Character');
+  }
+});
+
+// Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
