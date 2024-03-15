f038e5527545ecf531c841a72e0d7648dc2d521a
Arvid Alm <arvid.alm@iths.se>
2024-03-15 09:40:15 +0100

    Update index.js
    update new custom data

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ app.get('/', async (req, res) => {
 
 // * Code for Route 2 goes here
 
+app.get('/update-cobj', (req, res) => {
+    // rendera uppdatera data
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
