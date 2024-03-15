e3ee21d19dea4f77ad88c997445bbd76491f6785
Guerric SANT <guerricsant@MacBook-Air-de-Guerric.local>
2024-03-15 16:42:31 +0100

    First commit to my Integrating With HubSpot I: Foundations practicum repository.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const session = require('express-session');
+const querystring = require('querystring');
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -10,9 +12,30 @@ app.use(express.json());
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = '';
 
+const REDIRECT_URI = 'http://localhost/3000/contacts';
+
+
+const tokenStore = {};
+
+app.use(session({
+    secret: Math.random().toString(36).substring(2),
+    resave: false,
+    saveUninitialized: true
+}));
+
+const isAuthorized = (userId) => {
+    return tokenStore[userId] ? true : false;
+}
+
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+app.get('/', async (req, res) => {
+    if (isAuthorized(req.sessionID)) {
+
+    } else {
+        res.render("home", {authUrl});
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -20,7 +43,22 @@ const PRIVATE_APP_ACCESS = '';
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.get('/contacts', async (req, res) => {
+    res.send(req.query.code);
+    const authCodeProof = {
+        grant_type: 'authorization_code',
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+        redirect_uri: REDIRECT_URI,
+        code: req.query.code
+    };
+    try {
+        const responseBody = await axios.post('https://api.hubspot.com/oauth/v1/token', querystring.stringify(authCodeProof));
+        res.json(responseBody.data)
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
