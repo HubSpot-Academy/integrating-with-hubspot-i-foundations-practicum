b54e67030eacb2a53d2b9456f49c906c5d54497d
Guerric SANT <guerricsant@MacBook-Air-de-Guerric.local>
2024-03-19 16:59:58 +0100

    ended

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -10,12 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = '3368da6d-2c77-4d6b-915f-13fd5cf8ac05';
 
 const authUrl = "https://app-eu1.hubspot.com/oauth/authorize?client_id=5a58f855-a10c-4696-860b-f7f77b4cc312&redirect_uri=http://localhost:3000/practicum/&scope=crm.objects.contacts.read%20crm.schemas.custom.read%20crm.objects.custom.read"
-
-const REDIRECT_URI = 'http://localhost/3000/contacts';
-
 
 const tokenStore = {};
 
@@ -29,14 +27,30 @@ const isAuthorized = (userId) => {
     return tokenStore[userId] ? true : false;
 }
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+async function checkContactExists() {
 
-app.get('/', async (req, res) => {
-    if (isAuthorized(req.sessionID)) {
-       // const customObjectData = await callCustomObjectData();
-       res.render("contacts", { customObjectData });
-    } else {
-        res.render("contacts", {authUrl});
+}
+
+async function updateContactName(newName) {
+
+}
+
+async function createContact(newName) {
+
+}
+
+app.get('/practicum', async (req, res) => {
+    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        res.render('practicum', { title: 'Contacts | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
     }
 });
 
@@ -49,55 +63,108 @@ app.get('/update-cobj', async (req, res) => {
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 app.post('/update-cobj', async (req, res) => {
-    const formData = req.body
-    // Traitement des données formData...
+    const newName = req.body.newName
+
+    const contactExists = await checkContactExists();
+
+    if (contactExists) {
+        await updateContactName(newName)
+    } else {
+        await createContact(newName);
+    }
     res.redirect('/')
 })
 
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    };
-
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
-    }
-
-});
+// * Localhost
+app.listen(3000, () => console.log('Listening on http://localhost:3000/practicum'));
 */
 
+/* ----------------------------------------------- */
 
-// * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+require('dotenv').config();
+const axios = require('axios');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Hubspot services
+const hubspot_service = {
+  hubspotHeaders: {
+    headers: {
+      'Authorization': `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+      'Content-Type': 'application/json'
+    }
+  },
+  getObjects: async (objectId) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${objectId}?limit=100&archived=false&properties=name&properties=weight&properties=type`;
+
+    try {
+      const response = await axios.get(url, hubspot_service.hubspotHeaders);
+      return response.data.results;
+    } catch (error) {
+        console.log(error)
+      return;
+    }
+  },
+  updateObjects: async (objectId, formData) => {
+    const url = `https://api.hubapi.com/crm/v3/objects/${objectId}/`;
+    try {
+      const item = {
+        properties: {
+          "name": formData.name,
+          "weight": formData.weight,
+          "type": formData.type
+        }
+      };
+     
+      await axios.post(url, item, hubspot_service.hubspotHeaders);
+      return {
+        result: true
+      }
+    } catch (error) {
+      return {
+        result: false,
+        error
+      }
+    }
+  }
+}
+
+// tool service
+
+const tools_service = {
+    page_names: {
+        home: 'Dogs | Integrating With HubSpot I Practicum',
+        update: 'Dogs updated | Integrating With HubSpot I Practicum',
+    }
+}
+
+// index.js
+
+const CONTACT_ID = process.env.CONTACT_ID;
+
+app.get('/', async (req, res) => {
+    res.render('home', {
+        title: tools_service.page_names.home,
+        data: await hubspot_service.getObjects(CONTACT_ID)
+      }
+    )
+}
+);
+
+app.get('/updates', async (req, res) => res.render('updates', {
+    title: tools_service.page_names.update
+  }
+));
+
+app.post('/updates', async (req, res) => {
+  const request = await hubspot_service.updateObjects(CONTACT_ID, req.body);
+  if(request.result)  res.redirect('/');  
+  else res.status(500).send(request.error.response ? request.error.response.data : 'Error...');
+});
+
+app.listen(3000, () => console.log(`Listening on 3000`));
